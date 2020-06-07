e13a004cc6e844267bc1e747ade621982315a6e2
kaito2 <kaito.iwatsuki@nislab.doshisha.ac.jp>
2020-06-07 16:16:48 +0900

    puppeteer - js

---
+++ js/puppeteer/example.js
@@ -0,0 +1,10 @@
+// Get page and take screenshot.
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    await page.screenshot({ path: 'example.png' });
+    await browser.close();
+})();
